4719103796d22b43491ba2071e72f6db36c48d39
Maciej001 <m.nowakowskipl@gmail.com>
2015-06-24 16:03:09 +0000

    startDate FRAs works

--- client/templates/curve.js
+++ client/templates/curve.js
@@ -37,30 +37,24 @@ startDate = function(date) {
 	var spot = tPlusDate(todayDate, 2);
 	// if date is 1M 3M 9m 
 	if (/\d+(m|M)/.test(check_date) && !/\d+(m|M)[-]\d+(m|M)/.test(check_date)){
-		console.log('its month');
 		var month = Number(check_date.substring(0, check_date.length - 1));
 		start_date = spot;
 	} else if (/\d+(m|M)[-]\d+(m|M)/.test(check_date)) { // if FRAs
 		// if double digit starting periiod eg. 12m-15m
 		
 		if (_.isNumber(check_date.charAt(1))) {
-			// single digit as starting date -> 1m-4m
-			var start_month=Number(check_date.substring(0, 1));
+			// double digit as starting date -> 12m
+			var start_month=Number(check_date.substring(0, 2));
 		} else {
-			// double digit at start 
-			var start_month = Number(check_date.substring(0,2));
+			// single digit at start 
+			var start_month = Number(check_date.substring(0,1));
 		}
 		
 		var st_year = spot.getFullYear();
 		var st_month = spot.getMonth() + start_month;
-		var st_day = mFDate(spot).getDay();
+		var st_day = spot.getDate();
 		
-		console.log('its fra', start_month);
-		console.log('year ', st_year);
-		console.log('month ', st_month);
-		console.log('day ', st_day);
-		
-		start_date = new Date(st_year, st_month, st_day);
+		start_date = mFDate(new Date(st_year, st_month, st_day));
 		
 	} else if (/\d+(y|Y)/.test( check_date.replace(/ /g,'') ) ) { // if year eg. 1y, 5y 
 		console.log('its a year');
