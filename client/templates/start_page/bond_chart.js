3428c1ed0983c018b135902edaa680c53b2f5f06
Maciej001 <m.nowakowskipl@gmail.com>
2015-07-03 14:12:59 +0000

    x ordinal scale

--- client/templates/start_page/bond_chart.js
+++ client/templates/start_page/bond_chart.js
@@ -3,18 +3,25 @@ Template.bondChart.onRendered(function(){
     var crvData = _.map(curve.buckets, function(bucket) {
         return bucket
     });
+    crvData.length = curve.buckets.length;
     
     var myChart = {
         height:     400,
         width:      600,
         barWidth:   20,
         barOffset:  5,
-        background: '#f5f5f5'
+        background: '#f5f5f5',
+        barOuterPad: 0.2,
+        barPad: 0.1
     }
     
     var yScale = d3.scale.linear()
             .domain([0, d3.max(_.map(crvData, function(item) {return item.price}))]) // domain is the original range
             .range([0, myChart.height - 20])// domain is the range we want to fit into
+            
+    var xScale = d3.scale.ordinal()
+            .domain(d3.range(0, crvData.length))
+            .rangeRoundBands([0, myChart.width ], myChart.barPad, myChart.barOuterPad)
     
     d3.select('#chart').append('svg')
         .attr('width', myChart.width)
@@ -25,12 +32,12 @@ Template.bondChart.onRendered(function(){
         .enter()
         .append('rect')
             .style('fill', '#c61c6f')
-            .attr('width', myChart.barWidth)
+            .attr('width', xScale.rangeBand() )
             .attr('height', function(d){
                 return yScale(d.price);
             })
             .attr('x', function(d,i){
-                return i * (myChart.barWidth + myChart.barOffset)
+                return xScale(i) 
             })
             .attr('y', function(d){
                 return (myChart.height - yScale(d.price))
