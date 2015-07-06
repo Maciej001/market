dabdd6fed4dad6535f28382976ad764baa464b51
Maciej001 <m.nowakowskipl@gmail.com>
2015-07-06 08:10:49 +0000

    Color scale works

--- client/templates/start_page/bond_chart.js
+++ client/templates/start_page/bond_chart.js
@@ -15,19 +15,21 @@ Template.bondChart.onRendered(function(){
         barPad: 0.1
     };
     
-    var colors = d3.scale.linear()
-        .domain([0,  d3.max(_.map(crvData, function(item) {return item.price}))])
-        .range(["#FFB832", "#C61C6F"]);
-        
-    console.log('my color scale ', colors);
+    // var myColors = d3.scale.linear()
+    //     .domain([0,  d3.max(_.map(crvData, function(item) {return item.price}))])
+    //     .range(["#FFB832", "#C61C6F"]);
+    
+    var myColors = d3.scale.linear()
+        .domain([0, d3.max(_.map(crvData, function(item) {return item.price}))] )
+        .range(["#ffb832", "#c61c6f"]);
     
     var yScale = d3.scale.linear()
             .domain([0, d3.max(_.map(crvData, function(item) {return item.price}))]) // domain is the original range
-            .range([0, myChart.height - 20])// domain is the range we want to fit into
+            .range([0, myChart.height - 20]);// domain is the range we want to fit into
             
     var xScale = d3.scale.ordinal()
             .domain(d3.range(0, crvData.length))
-            .rangeRoundBands([0, myChart.width ], myChart.barPad, myChart.barOuterPad)
+            .rangeRoundBands([0, myChart.width ], myChart.barPad, myChart.barOuterPad);
     
     d3.select('#chart').append('svg')
         .attr('width', myChart.width)
@@ -37,7 +39,7 @@ Template.bondChart.onRendered(function(){
         .data(crvData)
         .enter()
         .append('rect')
-            .style('fill', colors)
+            .style('fill', function(d){ return myColors(d.price) })
             .attr('width', xScale.rangeBand() )
             .attr('height', function(d){
                 return yScale(d.price);
