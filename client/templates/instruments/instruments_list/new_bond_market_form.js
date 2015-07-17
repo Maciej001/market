8a2a2846a57fb2acbe74e5d6d2e9a22d467145af
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2015-07-17 15:22:56 +0000

    Reset inserBondMarket form validation

--- client/templates/instruments/instruments_list/new_bond_market_form.js
+++ client/templates/instruments/instruments_list/new_bond_market_form.js
@@ -1,23 +1,8 @@
 Template.insertBondMarketForm.events({
-    'submit form': function(e){
-		e.preventDefault();
-		var market = {
-			curve: 	    String($(e.target).find('#bondCurveName').val()).toUpperCase(),
-			currency: 	String($(e.target).find('#bondCurveCurr').val()).toUpperCase()
-		};
-		
-		Meteor.call('insertBondsMarketsItem', market, function(error, result) {
-		    if (error) {
-		        console.log(error);
-		    } else {
-    			Session.set('addBondToMarket', false);
-    			console.log('result', result);
-		    }
-		});
-	}, 
     'click .cancel': function(e){
     	e.preventDefault();
         Session.set('addBondToMarket',false);
+        AutoForm.resetForm('insertBondMarketForm'); // resets form validations
     }
     
 });
